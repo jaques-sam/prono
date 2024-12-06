9fd3c0fe2e0fdce1b622108e10d07336334d1c5c
Sam Jaques <s.jaques@televic.com>
2024-12-07 00:25:23 +0100

    build: add check for abundant crate deps

--- check.sh
+++ check.sh
@@ -8,4 +8,5 @@ cargo fmt --all -- --check
 cargo clippy --quiet --workspace --all-targets --all-features --  -D warnings -W clippy::all
 cargo test --quiet --workspace --all-targets --all-features
 cargo test --quiet --workspace --doc
+cargo machete
 trunk build

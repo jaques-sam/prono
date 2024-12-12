efd7aa9331be24cd1faa74c43b07f565f1c18ed5
Sam Jaques <s.jaques@televic.com>
2024-12-12 23:53:40 +0100

    feat: read secret environment vars (e.g. in GitHub actions) or read from secret file

--- src/ports/db.rs
+++ src/ports/db.rs
@@ -1,8 +1,10 @@
+use std::path::Path;
+
 mod answer;
 
 pub use answer::*;
 
 pub trait DB {
-    fn initialize(&self);
+    fn initialize(&self, config: &Path);
     fn add_answer(&mut self, user: u64, id: u16, answer: DbAnswer);
 }

dfea0a29f98bb1f5dd1563779b4c814cd4a48f4e
Sam Jaques <s.jaques@televic.com>
2024-12-22 00:15:50 +0100

    feat: first connection to database

--- src/adapters/mysql_proto_db.rs
+++ src/adapters/mysql_proto_db.rs
@@ -1,32 +1,42 @@
-use std::path::Path;
-
+use async_trait::async_trait;
+use eframe::Result;
 use log::info;
-// use mysql_async::prelude::*;
+use mysql_async::Opts;
+use tokio::runtime;
 
-use crate::{
-    proto_db::{DbAnswer, DB},
-    ReadConfig, SecureConfig,
-};
+use crate::proto_db::{self, DbAnswer, DB};
 
 static DB_NAME: &str = "db_proto";
-static CONFIG_FILENAME: &str = "secure_config.toml";
 
-#[derive(Default)]
-pub struct MysqlDb {}
+pub struct MysqlDb {
+    rt: runtime::Runtime,
+}
 
 impl MysqlDb {
-    pub fn new(secure_config_read: impl ReadConfig<SecureConfig>) -> Self {
-        let db = Self::default();
-        let secure_config = secure_config_read.read(Path::new(CONFIG_FILENAME));
-        db.initialize(secure_config);
+    pub fn new(secure_config: proto_db::Config) -> Self {
+        let db = Self {
+            rt: runtime::Builder::new_multi_thread().enable_all().build().unwrap(),
+        };
+        db.initialize(secure_config).expect("catch this error");
         db
     }
 }
 
+#[async_trait]
 impl DB for MysqlDb {
-    fn initialize(&self, _secure_config: SecureConfig) {
+    fn initialize(&self, secure_config: proto_db::Config) -> Result<()> {
         info!("Initializing Mysql db {DB_NAME}...");
+
+        self.rt.spawn(async move {
+            let database_url = secure_config.construct_url(DB_NAME);
+            let database_url = database_url.unsecure();
+            let pool = mysql_async::Pool::new(Opts::from_url(database_url).expect("catch this error"));
+
+            let mut _conn = pool.get_conn().await.expect("catch this error");
+        });
+
+        Ok(())
     }
 
-    fn add_answer(&mut self, _user: u64, _id: u16, _answer: DbAnswer) {}
+    async fn add_answer(&mut self, _user: u64, _id: u16, _answer: DbAnswer) {}
 }

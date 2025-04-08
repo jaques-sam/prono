ddf07593012db13e59e10d3d7832b75f1c3ba6b1
Sam Jaques <s.jaques@televic.com>
2025-04-09 00:08:25 +0200

    refactor: an answer is not mandatory in db

--- prono/src/ports/api.rs
+++ prono/src/ports/api.rs
@@ -7,7 +7,7 @@ pub use question::*;
 pub use survey::*;
 
 pub trait Surveys {
-    fn answer(&self, user: &str, question_id: u64) -> Answer;
+    fn answer(&self, user: &str, question_id: u64) -> Option<Answer>;
     fn response(&self, user: &str, survey_id: u64) -> Option<Survey>;
     fn add_answer(&mut self, user: &str, question_id: String, answer: Answer);
 }

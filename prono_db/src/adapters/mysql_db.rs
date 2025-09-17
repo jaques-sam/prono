483bcba5d6f303fbb9929c6c87890239943f1749
Sam Jaques <s.jaques@televic.com>
2025-09-17 14:30:52 +0200

    feat: first db implementations

--- prono_db/src/adapters/mysql_db.rs
+++ prono_db/src/adapters/mysql_db.rs
@@ -2,10 +2,10 @@ use log::info;
 use mysql_async::{prelude::*, Opts};
 use std::sync::Arc;
 
-use prono::api;
+use prono::api::{self, Answer};
 use tokio::{runtime, sync::Mutex};
 
-use crate::Config;
+use crate::{AnswerResponse, Config};
 
 struct State {
     connection: mysql_async::Conn,
@@ -76,12 +76,59 @@ impl MysqlDb {
 impl api::Surveys for MysqlDb {
     fn answer(&self, user: &str, question_id: u64) -> Option<api::Answer> {
         info!("DB: user {user} asks for answer for question id={question_id})");
-        todo!()
+        let state = self.state.clone();
+        let user = user.to_string();
+        let question_id = question_id.to_string();
+        self.rt.block_on(async move {
+            let mut lock = state.lock().await;
+            let state = lock.as_mut().expect("catch this error");
+
+            let row: Option<(String, String)> =
+                r"SELECT question_id, answer FROM AnswerResponse WHERE user = :user AND question_id = :question_id"
+                    .with(params! { "user" => user, "question_id" => question_id })
+                    .first(&mut state.connection)
+                    .await
+                    .expect("catch this error");
+
+            if let Some(row) = row {
+                return Some(Answer::from(row.1));
+            }
+
+            None
+        })
     }
 
     fn response(&self, user: &str, survey_id: u64) -> Option<api::Survey> {
         info!("DB: user {user} asks for response for survey id={survey_id})");
-        None
+        let state = self.state.clone();
+        let user = user.to_string();
+
+        self.rt.block_on(async move {
+            let mut lock = state.lock().await;
+            let state = lock.as_mut().expect("catch this error");
+
+            let rows: Vec<(String, String)> =
+                r"SELECT question_id, answer FROM AnswerResponse WHERE user = :user AND survey_id = :survey_id"
+                    .with(params! { "user" => user, "survey_id" => survey_id })
+                    .fetch(&mut state.connection)
+                    .await
+                    .expect("catch this error");
+
+            let mut questions = Vec::new();
+            for row in rows {
+                questions.push(api::Question {
+                    id: row.0,
+                    answer: Answer::from(row.1).into(),
+                    text: None,
+                });
+            }
+
+            Some(api::Survey {
+                id: survey_id,
+                description: None,
+                questions,
+            })
+        })
     }
 
     fn add_answer(&mut self, user: &str, question_id: String, answer: api::Answer) {

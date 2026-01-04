710c35410667758689761b085d276efb6161c5c4
Sam Jaques <s.jaques@televic.com>
2026-01-04 23:28:30 +0100

    refactor: remove duplicate call

--- prono/src/ports/prono.rs
+++ prono/src/ports/prono.rs
@@ -2,7 +2,6 @@ use crate::{Answer, Survey};
 
 pub trait Prono {
     fn empty_survey(&self) -> Survey;
-    fn filled_survey(&self, user: &str, survey_id: u64) -> Option<Survey>;
     fn add_answer(&mut self, user: &str, question_id: String, answer: Answer);
-    fn response(&self, user: &str, id: u64) -> Option<Survey>;
+    fn response(&self, user: &str, survey_id: u64) -> Option<Survey>;
 }
